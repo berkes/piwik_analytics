8b20717c8989fecad233c19bb0c5b21390e5d90c
Bèr Kessels <ber@berk.es>
2014-11-29 12:50:36 +0100

    Add tests for user_id.
    And fix failing tests

--- spec/views/piwik_tracking_tag_async.html.erb_spec.rb
+++ spec/views/piwik_tracking_tag_async.html.erb_spec.rb
@@ -9,13 +9,29 @@ describe "views/piwik_tracking_tag_async.html.erb" do
   it "should render url" do
     url = "example.com"
     id_site = nil
+    id_user = nil
     @template.result(binding).should =~ /http:\/\/example.com/
   end
 
   it "should render id_site" do
     url = nil
     id_site = 4
+    id_user = nil
     @template.result(binding).should =~ /'setSiteId', 4/
+  end
+
+  it "should render id_user" do
+    url = nil
+    id_site = nil
+    id_user = 1337
+    @template.result(binding).should =~ /_paq\.push\(\['setUserId', '1337'\]\);/
+  end
+
+  it "should not render id_user when it is not provided" do
+    url = nil
+    id_site = nil
+    id_user = nil
+    @template.result(binding).should_not =~ /_paq\.push\(\['setUserId', '1337'\]\);/
   end
 end
 
